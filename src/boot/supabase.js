09d6cbe5bcb61e22158248c7097d58a681dce0b8
Lherminez <mickael.lherminez@gmail.com>
2022-09-25 18:01:00 +0200

    UPDATE: Pages and authentication

--- src/boot/supabase.js
+++ src/boot/supabase.js
@@ -1,10 +1,15 @@
 import { createClient } from "@supabase/supabase-js";
+import useAuthUser from 'src/composables/UseAuthUser'
 
 const supabaseUrl = 'https://ryklfxgnqfohszqjzrhp.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5a2xmeGducWZvaHN6cWp6cmhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjM1MjE0MDksImV4cCI6MTk3OTA5NzQwOX0.K4epNnvx5FneYlHXGM8wD3g6T-Z98yKmCfpWFvaDciE'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-console. log ('init supabase:', supabase)
+supabase.auth.onAuthStateChange((event, session) => {
+  const { user } = useAuthUser ()
+
+  user.value = session.user || null
+})
 
 export default function useSupabase () {
   return { supabase }
---
+++ src/layouts/LoginLayout.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar class="text-black">
+        <q-toolbar-title>
+          Trombi
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'LoginLayout',
+
+  setup () {
+    return {
+    }
+  }
+})
+</script>
