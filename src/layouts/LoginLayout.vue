23b19e4bc116ac5bc4207d31015c7a6e9ed9f708
Lherminez <mickael.lherminez@gmail.com>
2022-09-30 19:55:47 +0200

    ADD: Add Add social media

--- src/layouts/LoginLayout.vue
+++ src/layouts/LoginLayout.vue
@@ -4,7 +4,7 @@
       <q-toolbar class="text-black">
         <q-toolbar-title>
           <q-btn v-if="$q.screen.lt.xs || $q.screen.lt.sm" :to="{ name: 'register' }" outline color="info">
-            <img alt="One Resources logo" src="~assets/One-resources-logo-short-r.png" style="height: 32px;">
+            <img alt="One Resources logo" src="~assets/One-resources-logo-short.svg" style="height: 32px;">
           </q-btn>
           <q-btn v-else :to="{ name: 'register' }" outline color="info">
             <img alt="One Resources logo" src="~assets/One-resources-logo.svg" style="width: 200px;">
@@ -21,20 +21,75 @@
 
     <q-footer elevated class="text-black" style="height:40px">
       <q-toolbar>
-        <q-toolbar-title v-if="!sizeXs && !sizeSm && !sizeMd" class="text-subtitle1 text-left col-md-2 col-lg-2 col-xl-1 q-mb-md">V{{version}}</q-toolbar-title>
+        <q-toolbar-title v-if="!sizeXs && !sizeSm && !sizeMd"
+          class="text-subtitle1 text-left col-md-2 col-lg-2 col-xl-1 q-mb-md">V{{version}}</q-toolbar-title>
         <q-toolbar-title class="text-center col-xs-12 col-sm-12 col-md-8 col-lg-8 col-xl-10 q-mb-md">
-          <q-btn flat round @click="goTo('https://www.objectware.fr/')" icon="mdi-web" />
-          <q-btn flat round @click="goTo('https://training.objectware.fr/')" icon="mdi-school" />
+          <q-btn flat round @click="goTo('https://www.objectware.fr/')">
+            <q-icon size="1.3rem">
+              <svg width="300" height="300" viewBox="0 0 300 300" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0_14_35)">
+                  <circle cx="150" cy="150" r="25" fill="black" />
+                  <circle cx="150" cy="150" r="140" stroke="black" stroke-width="20" />
+                </g>
+                <defs>
+                  <clipPath id="clip0_14_35">
+                    <rect width="300" height="300" fill="white" />
+                  </clipPath>
+                </defs>
+              </svg>
+            </q-icon>
+            <q-tooltip class="bg-accent text-black shadow-4 text-body2" size="2rem" :offset="[200, 20]"
+              transition-show="rotate" transition-hide="rotate">
+              Website Objectware
+            </q-tooltip>
+          </q-btn>
+          <q-btn flat round @click="goTo('https://training.objectware.fr/')" icon="mdi-school">
+            <q-tooltip class="bg-accent text-black shadow-4 text-body2" size="2rem" :offset="[200, 20]"
+              transition-show="rotate" transition-hide="rotate">
+              Website Objectware Training
+            </q-tooltip>
+          </q-btn>
           <q-btn flat round @click="goTo('https://www.linkedin.com/company/objectware/mycompany/verification/')"
-            icon="mdi-linkedin" />
-          <q-btn flat round @click="goTo('https://www.facebook.com/Objectware-486743308145905')" icon="mdi-facebook" />
-          <q-btn flat round @click="goTo('https://twitter.com/Objectware1')" icon="mdi-twitter" />
-          <q-btn flat round @click="goTo('https://objectware-fr.workplace.com/')" icon="mdi-facebook-workplace" />
-          <q-btn v-if="!sizeXs" flat round @click="goTo('https://www.instagram.com/objectware/')" icon="mdi-instagram" />
+            icon="mdi-linkedin">
+            <q-tooltip class="bg-accent text-black shadow-4 text-body2" size="2rem" :offset="[200, 20]"
+              transition-show="rotate" transition-hide="rotate">
+              Objectware on Linkedin
+            </q-tooltip>
+          </q-btn>
+          <q-btn flat round @click="goTo('https://www.facebook.com/Objectware-486743308145905')" icon="mdi-facebook">
+            <q-tooltip class="bg-accent text-black shadow-4 text-body2" size="2rem" :offset="[200, 20]"
+              transition-show="rotate" transition-hide="rotate">
+              Objectware on Facebook
+            </q-tooltip>
+          </q-btn>
+          <q-btn flat round @click="goTo('https://twitter.com/Objectware1')" icon="mdi-twitter">
+            <q-tooltip class="bg-accent text-black shadow-4 text-body2" size="2rem" :offset="[200, 20]"
+              transition-show="rotate" transition-hide="rotate">
+              Objectware on Twitter
+            </q-tooltip>
+          </q-btn>
+          <q-btn flat round @click="goTo('https://objectware-fr.workplace.com/')" icon="mdi-facebook-workplace">
+            <q-tooltip class="bg-accent text-black shadow-4 text-body2" size="2rem" :offset="[200, 20]"
+              transition-show="rotate" transition-hide="rotate">
+              Objectware on Workplace
+            </q-tooltip>
+          </q-btn>
+          <q-btn v-if="!sizeXs" flat round @click="goTo('https://www.instagram.com/objectware/')" icon="mdi-instagram">
+            <q-tooltip class="bg-accent text-black shadow-4 text-body2" size="2rem" :offset="[200, 20]"
+              transition-show="rotate" transition-hide="rotate">
+              Objectware on Instagram
+            </q-tooltip>
+          </q-btn>
           <q-btn v-if="!sizeXs" flat round @click="goTo('https://www.youtube.com/channel/UCZRfp0p4vC8WxbQVAymuZjw')"
-            icon="mdi-youtube" />
+            icon="mdi-youtube">
+            <q-tooltip class="bg-accent text-black shadow-4 text-body2" size="2rem" :offset="[200, 20]"
+              transition-show="rotate" transition-hide="rotate">
+              Objectware on Youtube
+            </q-tooltip>
+          </q-btn>
         </q-toolbar-title>
-        <q-toolbar-title v-if="!sizeXs && !sizeSm && !sizeMd" class="text-subtitle1 text-right col-md-2 col-lg-2 col-xl-1 q-mb-md">TeamJS©{{ year }}</q-toolbar-title>
+        <q-toolbar-title v-if="!sizeXs && !sizeSm && !sizeMd"
+          class="text-subtitle1 text-right col-md-2 col-lg-2 col-xl-1 q-mb-md">TeamJS©{{ year }}</q-toolbar-title>
       </q-toolbar>
     </q-footer>
   </q-layout>
